908373aaf694e339e2de32c35cc0df1f61134389
Damian Dudycz <damian.dudycz@gmail.com>
2022-02-19 06:55:19 +0100

    Moved dependencies

--- Sources/AtomicMVVM/ContainsDependencies.swift
+++
@@ -1,9 +0,0 @@
-// Use for dependency injection. For example include Coordinator, Services, etc.
-public protocol ContainsDependencies {
-    associatedtype Dependencies
-    var dependencies: Dependencies { get }
-}
-
-public extension ContainsDependencies where Dependencies == Void {
-    var dependencies: Void { Void() }
-}
